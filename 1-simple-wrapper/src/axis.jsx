2600044b36b45d1dad5c8cd25b00144995c417a1
AndyL <missedmemo@gmail.com>
2019-03-06 12:44:06 -0800

    display simple d3 axis

--- 1-simple-wrapper/src/axis.jsx
+++ 1-simple-wrapper/src/axis.jsx
@@ -1,3 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import * as d3 from 'd3'
 
-export default () => <rect width={100} height={ 100 } rx={15} ry={15} fill='purple' />
+class Axis extends Component {
+  gRef = React.createRef()
+
+  componentDidMount() {
+    this.renderD3()
+  }
+
+  componentDidUpdate() {
+    this.renderD3()
+  }
+
+  renderD3 = () => {
+    const scale = d3.scaleLinear()
+                    .domain([0,10])
+                    .range([0,200])
+    const axis = d3.axisBottom(scale)
+    d3.select(this.gRef.current).call(axis)
+  }
+
+  render() {
+    return <g transform='translate(10,30)' ref={ this.gRef } />
+  }
+}
+
+export default Axis
--- 1-simple-wrapper/src/index.js
+++ 1-simple-wrapper/src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import Axis from './axis.jsx'
 
-const App = () => <svg width={300} height={250} >
-  <Axis />
+const App = () => <svg width={800} height={400} >
+  <Axis y={10} />
 </svg>
 
 render(
