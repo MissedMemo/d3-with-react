6a6b377c7ecbe5bf7b819c302001dbed1b1465f4
AndyL <missedmemo@gmail.com>
2019-03-07 12:59:13 -0800

    add webpack to support more config options & styling

---
+++ 1-simple-wrapper/3A-scatterplot (static data)/webpack.config.js
@@ -0,0 +1,27 @@
+const { resolve } = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const cleanWebpackPlugin = require('clean-webpack-plugin')
+
+const SOURCE_DIR = resolve('src')
+
+module.exports = {
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      include: SOURCE_DIR,
+      loader: 'babel-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: 'index.html.template'
+    })
+  ]
+}
