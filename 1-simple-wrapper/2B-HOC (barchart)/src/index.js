7b876aba42c3d4e20a4e11f816bd0810c9b522cb
AndyL <missedmemo@gmail.com>
2019-03-07 11:10:36 -0800

    implement styled barchart with D3 blackbox HOC wrapper

--- 1-simple-wrapper/2B-HOC (barchart)/src/index.js
+++ 1-simple-wrapper/2B-HOC (barchart)/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Axis from './axis.jsx'
+import BarChart from './barchart.jsx'
 
-const App = () => <svg width={800} height={400} >
-  <Axis x={40} y={50} />
+const App = () => <svg width={960} height={600} id='svg' >
+  <BarChart x={40} y={50} width={600} height={400} />
 </svg>
 
 render(
