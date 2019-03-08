e9ea86602c54e1f67abb5f7e5ea365a36e22dd2a
AndyL <missedmemo@gmail.com>
2019-03-07 18:52:47 -0800

    pass scatterplot width & height as props

--- 1-simple-wrapper/3A-scatterplot (static data)/src/scatterplot.jsx
+++ 1-simple-wrapper/3A-scatterplot (static data)/src/scatterplot.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import * as d3 from 'd3'
 
-const xScale = d3.scaleLinear()
-                 .domain([0,1])
-                 .range([0,300])
+const ScatterPlot = ({x,y,data, width, height}) => {
 
-const yScale = d3.scaleLinear()
-                 .domain([0,1])
-                 .range([0,200])
+  const xScale = d3.scaleLinear()
+                   .domain([0,1])
+                   .range([0,width])
 
-const ScatterPlot = ({x,y,data}) =>
-  <g transform={ `translate( ${x}, ${y} )` } >
+  const yScale = d3.scaleLinear()
+                   .domain([0,1])
+                   .range([0,height])
+
+  return <g transform={ `translate( ${x}, ${y} )` } >
     { data.map( ([x,y]) =>
       <circle cx={xScale(x)} cy={yScale(y)} r="3" />
     )}
   </g>
+}
 
 export default ScatterPlot
